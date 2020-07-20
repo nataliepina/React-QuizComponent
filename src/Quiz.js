7e7915e4cee98c40746b7006906235a6c99787de
Natalie Pina <natalie.a.pina@gmail.com>
2020-07-20 16:31:22 -0500

    update ternary

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,8 +18,10 @@ class Quiz extends Component {
 
     return(
       <div>
-        {isQuizEnd && <QuizEnd />}
-        <QuizQuestion quiz_question={quizData.quiz_questions[(this.state.quiz_position - 1)]} />
+        {isQuizEnd 
+          ? <QuizEnd />
+          : <QuizQuestion quiz_question={quizData.quiz_questions[(this.state.quiz_position - 1)]} />
+        }
       </div>
     )
   }
